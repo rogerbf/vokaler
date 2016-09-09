0e40de132ebd83f261edcbda2dc7971439097ca9
rogerbf <roger@bergfalk.nu>
2016-09-09 19:30:31 +0200

    0.0.1

---
+++ README.md
@@ -0,0 +1,12 @@
+# vokaler
+
+Swedish vowels.
+
+``` javascript
+const vowels = require('vokaler')
+
+const all = vowels.all
+const front = vowels.front
+const raised = vowels.raised
+const retracted = vowels.retracted
+```
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const all = ['a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö']
+
+const front = ['i', 'y', 'long u', 'e', 'ä']
+
+const raised = ['ö', 'short u', 'short a']
+
+const retracted = ['o', 'å', 'long a']
+
+module.exports = {
+  all,
+  front,
+  raised,
+  retracted
+}
